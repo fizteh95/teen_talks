1b7181abf11e39cdfe2f4e9eb2c8a1bd41ed346b
fizteh95 <causeLove95@yandex.ru>
2021-02-08 21:35:38 +0500

    Победил загруженые картинки.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,26 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# from django.conf.urls import url
-# from django.urls import patterns
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-
-
-# if settings.DEBUG:
-#     urlpatterns += [
-#         '',
-#         url(
-#             r'^media/(?P<path>.*)$',
-#             'django.views.static.serve', {
-#                 'document_root': settings.MEDIA_ROOT,
-#             }
-#         ),
-#     ]
-
-# urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
