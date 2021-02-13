5e74c7793a48e6eb3689761603afb504349a6e30
fizteh95 <causeLove95@yandex.ru>
2021-02-13 10:19:03 +0500

    Доделал главную.

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 # blog/views.py
-from django.views.generic import ListView, DetailView # новое 
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
 class BlogListView(ListView):
+    paginate_by = 6
     model = Post
     template_name = 'home.html'
 
 
-class BlogDetailView(DetailView): # новое
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
