611db0d9ed9c1a66396895b96fdb8f6cb21fdc9f
fizteh95 <causeLove95@yandex.ru>
2021-02-17 14:08:41 +0500

    Дальнейшая работа с лого и пара страниц.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
  
 from .views import BlogListView, BlogDetailView
+from django.views.generic import TemplateView
  
 urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
+    path('contacts/', TemplateView.as_view(template_name="contacts.html"), name='contacts'),
     path('', BlogListView.as_view(), name='home'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
